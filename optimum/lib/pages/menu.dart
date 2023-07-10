adc85e2e960564f0e9963bd31aa64a9bba903ef2
Cherguelaine Oussama <lo_cherguelaine@esi.dz>
2023-07-10 14:54:17 +0200

    reglage de la bdd

--- optimum/lib/pages/menu.dart
+++ optimum/lib/pages/menu.dart
@@ -16,23 +16,20 @@ class _MenuState extends State<Menu> {
     final Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
       body: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/menu.png'),
-              fit: BoxFit.cover,
-            ),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/menu.png'),
+            fit: BoxFit.cover,
           ),
+        ),
         child: Column(
           children: <Widget>[
             Padding(
-              padding: EdgeInsets.fromLTRB(0, screenSize.height * 0.06, screenSize.width * 0.7, 0),
+              padding: EdgeInsets.fromLTRB(
+                  0, screenSize.height * 0.06, screenSize.width * 0.7, 0),
               child: FloatingActionButton(
                 onPressed: () {
-                  Navigator.pushAndRemoveUntil(
-                    context,
-                    MaterialPageRoute(builder: (context) => Home()),
-                        (Route<dynamic> route) => false,
-                  );
+                  Navigator.pop(context);
                 }, // Menu button
                 child: Icon(
                   Icons.close,
@@ -51,20 +48,23 @@ class _MenuState extends State<Menu> {
                 color: Colors.white,
               ),
             ),
-            SizedBox(height: screenSize.height * 0.08,),
+            SizedBox(
+              height: screenSize.height * 0.08,
+            ),
             Container(
               width: screenSize.width * 0.26,
               height: screenSize.width * 0.26,
               child: TextButton(
                 style: ButtonStyle(
                   backgroundColor: MaterialStateProperty.resolveWith<Color>(
-                        (Set<MaterialState> states) {
+                    (Set<MaterialState> states) {
                       return Colors.white;
                     },
                   ),
                   shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                     RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(screenSize.width * 0.15),
+                      borderRadius:
+                          BorderRadius.circular(screenSize.width * 0.15),
                       side: BorderSide(
                         color: Colors.black,
                       ),
@@ -77,7 +77,9 @@ class _MenuState extends State<Menu> {
                 ),
               ),
             ),
-            SizedBox(height: screenSize.height * 0.005,),
+            SizedBox(
+              height: screenSize.height * 0.005,
+            ),
             TextButton(
               onPressed: () {},
               child: Text(
@@ -91,122 +93,150 @@ class _MenuState extends State<Menu> {
               ),
             ),
             Padding(
-              padding: EdgeInsets.fromLTRB(screenSize.width * 0.15, screenSize.width * 0.1, screenSize.width * 0.15, 0),
+              padding: EdgeInsets.fromLTRB(screenSize.width * 0.15,
+                  screenSize.width * 0.1, screenSize.width * 0.15, 0),
               child: Column(
                 children: <Widget>[
-              SizedBox(height: screenSize.height * 0.005,),
-              Divider(
-                height: screenSize.height * 0.01,
-                color: Colors.grey[500],
-              ),
-              SizedBox(height: screenSize.height * 0.005,),
-              TextButton(
-                onPressed: () {},
-                child: Text(
-                  'Booking History',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: screenSize.width * 0.05,
-                    fontFamily: '0swald',
-                    letterSpacing: 1,
+                  SizedBox(
+                    height: screenSize.height * 0.005,
                   ),
-                ),
-              ),
-              SizedBox(height: screenSize.height * 0.005,),
-              Divider(
-                height: screenSize.height * 0.01,
-                color: Colors.grey[500],
-              ),
-              SizedBox(height: screenSize.height * 0.005,),
-              TextButton(
-                onPressed: () {},
-                child: Text(
-                  'Language',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: screenSize.width * 0.05,
-                    fontFamily: 'Oswald',
-                    letterSpacing: 1,
+                  Divider(
+                    height: screenSize.height * 0.01,
+                    color: Colors.grey[500],
                   ),
-                ),
-              ),
-              SizedBox(height: screenSize.height * 0.005,),
-              Divider(
-                height: screenSize.height * 0.01,
-                color: Colors.grey[500],
-              ),
-              SizedBox(height: screenSize.height * 0.005,),
-              TextButton(
-                onPressed: () {},
-                child: Text(
-                  'Dark Mode',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: screenSize.width * 0.05,
-                    fontFamily: 'Oswald',
-                    letterSpacing: 1,
+                  SizedBox(
+                    height: screenSize.height * 0.005,
                   ),
-                ),
-              ),
-              SizedBox(height: screenSize.height * 0.005,),
-              Divider(
-                height: screenSize.height * 0.01,
-                color: Colors.grey[500],
-              ),
-              SizedBox(height: screenSize.height * 0.005,),
-              TextButton(
-                onPressed: () {},
-                child: Text(
-                  'Help',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: screenSize.width * 0.05,
-                    fontFamily: 'Oswald',
-                    letterSpacing: 1,
-                  ),
-                ),
-              ),
-              SizedBox(height: screenSize.height * 0.005,),
-              Divider(
-                height: screenSize.height * 0.01,
-                color: Colors.grey[500],
-              ),
-              SizedBox(height: screenSize.height * 0.005,),
-              TextButton(
-                onPressed: () async {
-                  await AuthService.signOut();
-                  Navigator.pushAndRemoveUntil(
-                    context,
-                    MaterialPageRoute(builder: (context) => Start()),
-                        (Route<dynamic> route) => false, // Supprime toutes les routes précédentes
-                  );
-                },
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Icon(
-                      Icons.logout,
-                      color: Color(0xFFD37777),
-                    ),
-                    SizedBox(width: screenSize.width * 0.02,),
-                    Text(
-                      'Log Out',
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Booking History',
                       style: TextStyle(
-                        color: Color(0xFFD37777),
+                        color: Colors.black,
                         fontSize: screenSize.width * 0.05,
-                        fontFamily: 'Poppins',
+                        fontFamily: '0swald',
                         letterSpacing: 1,
                       ),
                     ),
-                  ],
-                ),
-              ),
-              SizedBox(height: screenSize.height * 0.005,),
-              Divider(
-                height: screenSize.height * 0.01,
-                color: Colors.grey[500],
-              ),
-              SizedBox(height: screenSize.height * 0.005,),
+                  ),
+                  SizedBox(
+                    height: screenSize.height * 0.005,
+                  ),
+                  Divider(
+                    height: screenSize.height * 0.01,
+                    color: Colors.grey[500],
+                  ),
+                  SizedBox(
+                    height: screenSize.height * 0.005,
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Language',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: screenSize.width * 0.05,
+                        fontFamily: 'Oswald',
+                        letterSpacing: 1,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: screenSize.height * 0.005,
+                  ),
+                  Divider(
+                    height: screenSize.height * 0.01,
+                    color: Colors.grey[500],
+                  ),
+                  SizedBox(
+                    height: screenSize.height * 0.005,
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Dark Mode',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: screenSize.width * 0.05,
+                        fontFamily: 'Oswald',
+                        letterSpacing: 1,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: screenSize.height * 0.005,
+                  ),
+                  Divider(
+                    height: screenSize.height * 0.01,
+                    color: Colors.grey[500],
+                  ),
+                  SizedBox(
+                    height: screenSize.height * 0.005,
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Help',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: screenSize.width * 0.05,
+                        fontFamily: 'Oswald',
+                        letterSpacing: 1,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: screenSize.height * 0.005,
+                  ),
+                  Divider(
+                    height: screenSize.height * 0.01,
+                    color: Colors.grey[500],
+                  ),
+                  SizedBox(
+                    height: screenSize.height * 0.005,
+                  ),
+                  TextButton(
+                    onPressed: () async {
+                      await AuthService.signOut();
+                      Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(builder: (context) => Start()),
+                        (Route<dynamic> route) =>
+                            false, // Supprime toutes les routes précédentes
+                      );
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.logout,
+                          color: Color(0xFFD37777),
+                        ),
+                        SizedBox(
+                          width: screenSize.width * 0.02,
+                        ),
+                        Text(
+                          'Log Out',
+                          style: TextStyle(
+                            color: Color(0xFFD37777),
+                            fontSize: screenSize.width * 0.05,
+                            fontFamily: 'Poppins',
+                            letterSpacing: 1,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: screenSize.height * 0.005,
+                  ),
+                  Divider(
+                    height: screenSize.height * 0.01,
+                    color: Colors.grey[500],
+                  ),
+                  SizedBox(
+                    height: screenSize.height * 0.005,
+                  ),
                 ],
               ),
             ),
--- optimum/lib/services/database.dart
+++ optimum/lib/services/database.dart
@@ -5,36 +5,18 @@ class DatabaseService {
   // collection reference
   final String uid;
   DatabaseService({required this.uid});
-  final CollectionReference optimumCollection =FirebaseFirestore.instance.collection('users');
+  static final CollectionReference usersCollection =
+      FirebaseFirestore.instance.collection('users');
   Future updateUserData(String name, String lastName) async {
-    return await optimumCollection.doc(uid).set({
-      'name' : name,
-      'lastname' : lastName,
-
+    return await usersCollection.doc(uid).set({
+      'name': name,
+      'lastname': lastName,
     });
   }
 
-  // get nom d'utilisateur
+  // get  users streams
 
-  Future<String> getUserName() async {
-    User? user = FirebaseAuth.instance.currentUser;
-    if (user != null) {
-      String uid = user.uid;
-      DocumentSnapshot<Map<String, dynamic>> snapshot = await FirebaseFirestore.instance
-          .collection('users')
-          .doc(uid)
-          .get();
-
-      if (snapshot.exists) {
-        String name = snapshot.get('name');
-        return name;
-      } else {
-        print('L\'utilisateur n\'existe pas dans Firestore');
-        return '';
-      }
-    } else {
-      print('Aucun utilisateur connecté');
-      return '';
-    }
+  static Stream<QuerySnapshot> get user {
+    return usersCollection.snapshots();
   }
 }
